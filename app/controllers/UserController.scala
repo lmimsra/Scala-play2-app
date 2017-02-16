4ce6a9de3a86321cc606bbdc70ba6c6305bc368f
mmr.K <pollvaulter.m@gmail.com>
2017-02-16 17:05:25 +0900

    フォームを改良

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -8,14 +8,17 @@ import play.api.data.Form
 /**
   * Created by mmr on 2017/02/10.
   */
-case class User(name: String, email: String, age: Int)
+case class User(name: String, age: Int, sex: String, other: Other)
+case class Other(hobby: String, place: String, birthday: String)
 
 @Singleton
 class UserController extends Controller {
-  val form = Form( "name" -> text )
+ // val form = Form( "name" -> text )
+  val form = Form[User](mapping("name" -> text, "age" -> number, "sex" -> text,
+    "other" ->mapping("hobby" -> text,"place" -> text, "birthday" -> text)(Other.apply)(Other.unapply))(User.apply)(User.unapply))
   def outputPage = Action{implicit request =>
-    val Username =form.bindFromRequest.get
-    Ok(views.html.output(Username))
+    val inUser =form.bindFromRequest.get
+    Ok(views.html.output(inUser.name,inUser.age,inUser.sex,inUser.other.place))
   }
 
 }
---
+++ public/javascripts/main.js
@@ -0,0 +1,25 @@
+if (window.console) {
+  console.log("Welcome to your Play application's JavaScript!");
+}
+
+//入力ページへ
+function gotoInput() {
+  location.href="/input";
+}
+//トップページへ
+function gotoTop() {
+  location.href="/";
+}
+
+//デフォルトのようこそページへ
+function gotoWellcome() {
+  location.href="/wellcome";
+}
+//カウンターデモ
+function gotoCount() {
+  location.href="/count"
+}
+//メッセージの表示
+function gotoMessage() {
+  location.href="/message"
+}
